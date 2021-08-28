2a576e545c4fdce98c8e8e0b2dba4b27e7156c99
Iago Tudela Díaz <iagotudela@gmail.com>
2021-08-28 16:13:32 +0200

    First commit. UFO API configured

---
+++ ufo-hunters-api/src/test/java/com/iagotd/ufohuntersapi/UfoHuntersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iagotd.ufohuntersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfoHuntersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
